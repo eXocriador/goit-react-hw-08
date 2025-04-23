97bc9c3681dcadaaf0006f942c6f89934ff5e70b
Oleh Tatarynov <exocriador@gmail.com>
2025-04-23 17:27:57 +0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import { store, persistor } from "./redux/store";
+import { store } from "./redux/store";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
