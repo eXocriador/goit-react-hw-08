490ec06e3caa3f62e1e2d377b49803f26f0aea7b
Oleh Tatarynov <exocriador@gmail.com>
2025-04-27 04:21:18 +0300

    update components

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,37 +1,22 @@
 import PropTypes from "prop-types";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from "@mui/icons-material/Edit";
+
 import styles from "./ContactList.module.css";
 
 const ContactList = ({ contacts, onDelete, onEdit }) => {
   return (
     <List className={styles.list}>
       {contacts.map(({ id, name, number }) => (
-        <ListItem key={id} className={styles.contactItem}>
-          <Box className={styles.contactText}>
-            <Typography className={styles.contactName}>{name}</Typography>
-            <Typography className={styles.contactNumber}>{number}</Typography>
-          </Box>
-          <Box className={styles.contactActions}>
-            <IconButton
-              className={styles.editButton}
-              onClick={() => onEdit(id)}
-            >
-              <EditIcon fontSize="small" />
-            </IconButton>
-            <IconButton
-              className={styles.deleteButton}
-              onClick={() => onDelete(id)}
-            >
-              <DeleteIcon fontSize="small" />
-            </IconButton>
-          </Box>
-        </ListItem>
+        <li key={id}>
+          <div>{name}</div>
+          <div>{number}</div>
+          <button type="button" onClick={() => onEdit({ id, name, number })}>
+            ✏️
+          </button>
+          <button type="button" onClick={() => onDelete(id)}>
+            🗑️
+          </button>
+        </li>
       ))}
     </List>
   );
